252188579b3919a2e465063d14d0ef7d9febe934
Lance Fono <lance.fono@digital14.com>
2023-01-31 06:06:06 +0200

    Remove previous placeholder

--- pkcs11/app.js
+++ pkcs11/app.js
@@ -3,28 +3,7 @@ const pkcs11ObjectType = {
     PublicKey: Symbol("publicKey")
 }
 
-module.exports.templateTags = [{
-  name: 'randomInteger',
-  displayName: 'Random Integer',
-  description: 'Generate a random integer.',
-  args: [
-      {
-          displayName: 'Minimum',
-          description: 'Minimum potential value',
-          type: 'number',
-          defaultValue: 0
-      }, 
-      {
-          displayName: 'Maximum',
-          description: 'Maximum potential value',
-          type: 'number',
-          defaultValue: 100
-      }
-  ],
-  async run (context, min, max) {
-      return Math.round(min + Math.random() * (max - min));
-  }
-},
+module.exports.templateTags = [
 {
     name: 'getCertificate',
     displayName: 'Get PKCS11 Certificate',
