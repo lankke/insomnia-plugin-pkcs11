cb8a4e402e4fd55761543ac0ad25fcb67cc594aa
Lance Fono <lance.fono@digital14.com>
2023-01-31 06:04:48 +0200

    Add skeleton for grabbing certificate

--- pkcs11/app.js
+++ pkcs11/app.js
@@ -1,7 +1,8 @@
-/**
- * Example template tag that generates a random number 
- * between a user-provided MIN and MAX
- */
+const pkcs11ObjectType = {
+    Certificate: Symbol("certificate"),
+    PublicKey: Symbol("publicKey")
+}
+
 module.exports.templateTags = [{
   name: 'randomInteger',
   displayName: 'Random Integer',
@@ -23,4 +24,44 @@ module.exports.templateTags = [{
   async run (context, min, max) {
       return Math.round(min + Math.random() * (max - min));
   }
+},
+{
+    name: 'getCertificate',
+    displayName: 'Get PKCS11 Certificate',
+    args: [
+        {
+            displayName: 'Slot Id',
+            description: 'Id of the Cryptoki Slot you want to query (default: 0)',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'PIN Number',
+            description: 'PIN Number to your USB Token',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'PKCS11 Object Type',
+            description: 'The type of object you want to fetch from the token (certificate | publicKey)',
+            type: 'enum',
+            defaultValue: pkcs11ObjectType.Certificate,
+            options:[
+                {
+                    displayName: "X509 Certificate",
+                    value: "certificate"
+                },
+                {
+                    displayName: "Public Key",
+                    value: "publicKey"
+                }
+            ]
+        }
+
+    ], async run (context, slotId, pinNumber, objectType){
+        console.log(slotId,pinNumber,objectType);
+        return "Nice";
+    }
+
 }];
+
