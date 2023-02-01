bea66ecf8500e884649625a223a4554c8f143e2c
Lance Fono <lance.fono@digital14.com>
2023-02-01 15:20:12 +0200

    Add pkcs11js library and calls

--- pkcs11/app.js
+++ pkcs11/app.js
@@ -1,6 +1,21 @@
+var pkcs11 = require('pkcs11js');
+
 const pkcs11ObjectType = {
     Certificate: Symbol("certificate"),
     PublicKey: Symbol("publicKey")
+}
+
+const OPENSC_PKCS11_LIB_PATH = "/usr/lib/opensc-pkcs11.so";
+
+function getObjectTemplate(label){
+    
+    var certificateTemplate = [
+        { type: pkcs11js.CKA_CLASS, value: pkcs11js.CKO_CERTIFICATE },
+        { type: pkcs11js.CKA_TOKEN, value: false },
+        { type: pkcs11js.CKA_LABEL, value: label }
+    ];
+
+    return certificateTemplate;
 }
 
 module.exports.templateTags = [
@@ -39,7 +54,66 @@ module.exports.templateTags = [
 
     ], async run (context, slotId, pinNumber, objectType){
         console.log(slotId,pinNumber,objectType);
-        return "Nice";
+        
+        var isInitialized = false;
+        var pkcs11 = new pkcs11js.PKCS11();
+        var rv;
+
+        try {
+            pkcs11.load(OPENSC_PKCS11_LIB_PATH);
+            pkcs11.C_Initialize();
+
+            // Getting list of slots
+            var slots = pkcs11.C_GetSlotList(true);
+            var slot = slots[0];
+
+            // Getting info about slot
+            var slot_info = pkcs11.C_GetSlotInfo(slot);
+            // Getting info about token
+            var token_info = pkcs11.C_GetTokenInfo(slot);
+
+            // Getting info about Mechanism
+            var mechs = pkcs11.C_GetMechanismList(slot);
+            var mech_info = pkcs11.C_GetMechanismInfo(slot, mechs[0]);
+
+            var session = pkcs11.C_OpenSession(slot, pkcs11js.CKF_RW_SESSION | pkcs11js.CKF_SERIAL_SESSION);
+
+            // Getting info about Session
+            var info = pkcs11.C_GetSessionInfo(session);
+            pkcs11.C_Login(session, 1, pinNumber);
+
+            var certTemplate = getObjectTemplate(label);
+
+            pkcs11.C_FindObjectsInit(session, certTemplate);
+
+            var hObject = pkcs11.C_FindObjects(session);
+
+            if(hObject){
+                console.log("Object Found!");
+                var attr = pkcs11.C_GetAttributeValue(session, hObject,[
+                    {type: pkcs11.CKA_VALUE }
+                ]);
+
+                console.log("attr = ",attr);
+
+            }else{
+                console.error("No object found!");
+            }
+
+            pkcs11.C_FindObjectsFinal(session);
+            
+            pkcs11.C_Logout(session);
+            pkcs11.C_CloseSession(session);
+            
+        } catch (error) {
+            console.error("Exception Caught", error);
+        } finally{
+            if(isInitialized){
+                pkcs11.C_Finalize();
+            }
+        }
+
+        return "Here we go";
     }
 
 }];
